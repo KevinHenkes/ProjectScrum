d46c596e6de5906d01dd8dfdcf761c75f3330d04
Emeline Hourmand <shiro-x3@hotmail.fr>
2017-06-21 16:24:00 +0200

    Print result in File

--- src/core/Application.java
+++ src/core/Application.java
@@ -1,48 +1,62 @@
 package core;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	
-	boolean flag = true;
-	
-	do {
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Bienvenue dans la super calculette du G4 !");
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-	    System.out.println("Veuillez entrer le premier chiffre :");
-	    int a = Integer.parseInt(sc.nextLine());
+		boolean flag = true;
+		int result = 0;
 
-	    System.out.println("Veuillez entrer le deuxième chiffre :");
-	    int b = Integer.parseInt(sc.nextLine());
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Bienvenue dans la super calculette du G4 !");
 
-	    System.out.println("Veuillez saisir l'opérateur : (+ - / *)");
-	    String operator = sc.nextLine();
+			System.out.println("Veuillez entrer le premier chiffre :");
+			int a = Integer.parseInt(sc.nextLine());
 
-	    switch (operator) {
-	    case "+":
-		System.out.println(a + b);
-		break;
-	    case "-":
-		System.out.println(a - b);
-		break;
-	    case "*":
-		System.out.println(a * b);
-		break;
-	    case "/":
-		System.out.println(a / b);
-		break;
-	    }
-	    System.out.println("Continuer ? O/N");
-	    String input = sc.nextLine();
+			System.out.println("Veuillez entrer le deuxième chiffre :");
+			int b = Integer.parseInt(sc.nextLine());
 
-	    if (input.equals("N"))
-		flag = false;
-	} while (flag);
+			System.out.println("Veuillez saisir l'opérateur : (+ - / *)");
+			String operator = sc.nextLine();
 
-    }
+			switch (operator) {
+			case "+":
+				System.out.println(a + b);
+				result = a + b;
+				break;
+			case "-":
+				System.out.println(a - b);
+				result = a - b;
+				break;
+			case "*":
+				System.out.println(a * b);
+				result = a * b;
+				break;
+			case "/":
+				System.out.println(a / b);
+				result = a / b;
+				break;
+			}
+			System.out.println("Continuer ? O/N");
+			String input = sc.nextLine();
+
+			if (input.equals("N"))
+				flag = false;
+		} while (flag);
+
+		try {
+			PrintWriter writer = new PrintWriter("resultat.txt", "UTF-8");
+			writer.println(result);
+			writer.close();
+		} catch (IOException e) {
+			// do something
+		}
+	}
 
 }
